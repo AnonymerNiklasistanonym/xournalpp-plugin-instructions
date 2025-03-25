f2d8b874a6e7c24e70f9b0a654eb18a389cff0c8
Niklas <niklas.mikeler@gmail.com>
2025-03-25 11:55:10 +0100

    Simplify the creation of multiple toolbar entries by creating global functions
    Reduces code lines and code complexity

--- examples/BrowserShortcut/main.lua
+++ examples/BrowserShortcut/main.lua
@@ -1,19 +1,36 @@
-local url = "https://www.google.com/"
+local urls = {
+    -- add custom entries to this list (provide a 'icon-$ID.png/.svg' file as icon!)
+    -- { url = "https://www.github.com", id = "github", name = "GitHub" },
+    {url = "https://www.google.com", id = "bookshelf", name = "Google"},
+}
 
-function initUi()
-    -- register menu bar entry and toolbar icon
-    app.registerUi({
-        ["menu"] = "Open " .. url .. " in browser", -- menu bar entry/tooltip text
-        ["callback"] = "run", -- function to run on click
-        ["toolbarId"] = "BROWSER_SHORTCUT", -- toolbar ID
-        ["iconName"] = "icon-bookshelf", -- the icon ID
-    })
+local function openUrl(url)
+    -- open URL in the default browser
+
+    local command
+    if package.config:sub(1, 1) == "\\" then
+        -- Windows
+        command = "start " .. url
+    else
+        -- Linux
+        command = "xdg-open " .. url
+    end
+
+    os.execute(command)
 end
 
-function run()
-    -- open URL in the default browser
-    -- > Windows
-    os.execute("start " .. url)
-    -- > Linux
-    os.execute("xdg-open " .. url)
+function initUi()
+    -- register menu bar entries and toolbar icons
+    for i, site in ipairs(urls) do
+        -- dynamically create a global function for each site
+        _G["run_" .. i] = function() openUrl(site.url) end
+
+        -- register menu bar entry and toolbar icon
+        app.registerUi({
+            ["menu"] = "Open " .. site.name .. " in browser", -- menu bar entry/tooltip text
+            ["callback"] = "run_" .. i, -- function to run on click
+            ["toolbarId"] = "BROWSER_" .. site.id:upper() .. "_SHORTCUT", -- toolbar ID
+            ["iconName"] = "icon-" .. site.id, -- the icon ID
+        })
+    end
 end
--- examples/ChangeBackground/main.lua
+++ examples/ChangeBackground/main.lua
@@ -3,38 +3,24 @@ local defaultBackgroundValues = {
     "plain", "lined", "ruled", "graph", "dotted", "isodotted", "isograph",
 }
 
-function initUi()
-    -- register menu bar entries and toolbar icons
-
-    for _, bgValue in ipairs(defaultBackgroundValues) do
-        local toolbarId = "CHANGE_BACKGROUND_TO_" .. string.upper(bgValue) ..
-                              "_SHORTCUT"
-        local iconName = "icon-change-background-to-" .. bgValue
-
-        app.registerUi({
-            ["menu"] = "Update background to " .. bgValue, -- menu bar entry/tooltip text
-            ["callback"] = "run_" .. bgValue,
-            ["toolbarId"] = toolbarId, -- toolbar ID
-            ["iconName"] = iconName, -- the icon ID
-        })
-    end
-end
-
-local function run(bgValue)
+local function changeBg(bgValue)
     -- set the background to dots of current page
     app.changeCurrentPageBackground(bgValue)
 end
 
-function run_dotted() run("dotted") end
+function initUi()
+    -- register menu bar entries and toolbar icons
+    for _, bgValue in ipairs(defaultBackgroundValues) do
+        -- dynamically create a global function for each background
+        _G["run_" .. bgValue] = function() changeBg(bgValue) end
 
-function run_plain() run("plain") end
-
-function run_lined() run("lined") end
-
-function run_ruled() run("ruled") end
-
-function run_graph() run("graph") end
-
-function run_isodotted() run("isodotted") end
-
-function run_isograph() run("isograph") end
+        -- register menu bar entry and toolbar icon
+        app.registerUi({
+            ["menu"] = "Update background to " .. bgValue, -- menu bar entry/tooltip text
+            ["callback"] = "run_" .. bgValue, -- function to run on click
+            ["toolbarId"] = "CHANGE_BACKGROUND_TO_" .. bgValue:upper() ..
+                "_SHORTCUT", -- toolbar ID
+            ["iconName"] = "icon-change-background-to-" .. bgValue, -- the icon ID
+        })
+    end
+end
